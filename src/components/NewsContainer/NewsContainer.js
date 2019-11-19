15b8d00086fd99ae40823a325c6c1b8beb25e4f7
James Miller <james.rex.miller.4@gmail.com>
2019-11-19 14:03:33 -0700

    Add NewsContainer component and styles

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,25 @@
 import React from 'react';
-import './NewsContainer.css'
+import './NewsContainer.css';
+import NewsArticle from '../NewsArticle/NewsArticle.js';
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = ({local}) => {
+    const articles = local.map(article => {
+        return (
+            <NewsArticle
+                key = {article.id}
+                image = {article.img}
+                title = {article.title}
+                description = {article.description}
+                url = {article.url}
+            />
+        )
+    })
+
+    return (
+        <div className="news-container">
+            {articles}
+        </div>
+    )
+}
 
 export default NewsContainer;
