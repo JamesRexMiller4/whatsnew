4fe522c4a539615f18a578de85c9ce5096efdbce
James Miller <james.rex.miller.4@gmail.com>
2019-11-19 14:07:16 -0700

    Remove unused images and clear out ReadMe

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = () => {
+    return(
+        <aside>
+            <
+        </aside>
+    )
+}
 
 export default Menu;
