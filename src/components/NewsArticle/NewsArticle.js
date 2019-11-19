fff318bf8e9832faa869ee7e30f9b55f1067e611
James Miller <james.rex.miller.4@gmail.com>
2019-11-19 14:02:21 -0700

    Add NewsArticle component and styles

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = ({id, headline, image, description, url}) => {
+    return (
+        <div className='news-card-div'>
+          <img src={image} className='article-image' alt='a cat sits on a tree'/>
+          <h2>{headline}</h2>
+          <p className='article-paragraph'>{description}</p>
+          <a className='article-anchor' href={url}>Link to Article</a>
+        </div>
+    )
+}
 
 export default NewsArticle;
