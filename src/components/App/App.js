0a4f33605a18212bcffcfe4c533bd212f82bb895
James Miller <james.rex.miller.4@gmail.com>
2019-11-20 02:19:14 -0700

    Add chooseNews method, update state to include all datasets

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer';
+import Menu from '../Menu/Menu';
+import Header from '../Header/Header';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local : local
+      displayNews : [
+        ...local, 
+        ...entertainment, 
+        ...technology, 
+        ...science, 
+        ...health
+      ]
     }
+    this.chooseNews = this.chooseNews.bind(this)
+  }
+
+  chooseNews(id) {
+    let news = id
+    news === 'entertainment' ? news = entertainment 
+    : id === 'health' ? news = health
+    : id === 'science' ? news = science
+    : id === 'technology' ? news = technology : news = local
+    this.setState({displayNews: news})
   }
 
   render() {
     return (
       <div className="app">
-        <NewsContainer local= {this.state.local}/>
+        <Header />
+        <Menu chooseNews={this.chooseNews}/>
+        <NewsContainer displayNews= {this.state.displayNews}/>
       </div>
     );
   }
