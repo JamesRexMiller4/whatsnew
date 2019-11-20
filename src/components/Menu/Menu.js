135e3c8908c1f39f8612d95a7ff11a5d8fc8d366
James Miller <james.rex.miller.4@gmail.com>
2019-11-20 02:18:40 -0700

    Add onclick functionality to buttons

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,11 +4,11 @@ import './Menu.css';
 const Menu = ({chooseNews}) => {
     return(
         <nav>
-            <button type='button' onClick={chooseNews} className='btn'>Local News</button>
-            <button type='button' onClick={chooseNews} className='btn'>Technology</button>
-            <button type='button' onClick={chooseNews} className='btn'>Entertainment</button>
-            <button type='button' onClick={chooseNews} className='btn'>Science</button>
-            <button type='button' onClick={chooseNews} className='btn'>Health</button>
+            <button type='button' onClick={() => chooseNews('local')} id='local' className='btn'>Local News</button>
+            <button type='button' onClick={() => chooseNews('technology')} id='technology' className='btn'>Technology</button>
+            <button type='button' onClick={() => chooseNews('entertainment')} id='entertainment' className='btn'>Entertainment</button>
+            <button type='button' onClick={() => chooseNews('science')} id='science' className='btn'>Science</button>
+            <button type='button' onClick={() => chooseNews('health')} id='health' className='btn'>Health</button>
         </nav>
     )
 }
